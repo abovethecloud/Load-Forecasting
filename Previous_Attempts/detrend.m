71f27ebb1b1a8859757a8772a4ab439b94894090
abovethecloud <claudio.peroni93@gmail.com>
2015-07-08 16:30:53 +0200

    Commentato lo script di detrendizzazione (nonLog)

--- Previous_Attempts/detrend.m
+++ Previous_Attempts/detrend.m
@@ -3,15 +3,27 @@ clear all
 close all
 
 load datiOTT;
+
+% Do i nomi alle colonne dei dati che mi interessano
 loads = datiOTT(:,2);
 years = datiOTT(:,3);
 
+% Per ogni anno, calcolo la media dei carichi sulle 4 settimane di Ottobre
+% e la salva negli elementi del vettore "means"
 for i = 0:11
+    
     currentYear = 2000+i;
-    currentLoads = loads(years == currentYear);
-    fourWeeksLoads = currentLoads(1:28); % mi procuro i dati robusti (28 giorni invece di 31)
+    booleanVectorCurrentYear = (years == currentYear);
+    currentYearLoads = loads(booleanVectorCurrentYear);
+    
+    % Prendo solo i dati dei primi 28 giorni per l'Ottobre di ogni anno.
+    % Questo per avere esattamente 4 settimane ed evitare di avere 5 domeniche
+    % o cose del genere.. Perdo in robustezza (se salta un dato: casino!)
+    fourWeeksLoads = currentYearLoads(1:28);
     means(i+1) = mean(fourWeeksLoads);
+    
 end
 
-figure(1)
+% Plotto il grafico del trend negli anni
+figure('Name', 'Data Trend',    'NumberTitle', 'off')
 plot(2000:2011, means, 'r')
