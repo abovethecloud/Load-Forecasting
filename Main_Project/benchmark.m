0b1a4fa96a301494871705cc32412ad8ccb9903c
abovethecloud <claudio.peroni93@gmail.com>
2015-07-14 14:24:02 +0200

    IMPORTANTE: Corretto algoritmo di benchmark.
    Precedentemente l'algoritmo conteneva un grave bug: il vettore che conteneva
    l'errore MAPE era inizializzato più lungo di quanto non risultasse essere in reltà il numero di errori. Come conseguenza l'errore medio risultava più basso per la presenza di zeri artefatti.

--- Main_Project/benchmark.m
+++ Main_Project/benchmark.m
@@ -1,32 +1,47 @@
-%% Benchmark:
-% Calcola l'errore di predizione per tutte le settimane dell'Ottobre 2011 e
-% ne calcola la media.
+%% BENCHMARK:
+% Calcola l'errore di predizione per tutte le settimane dell'Ottobre di un
+% anno specificato (usato come validazione) e ne calcola la media.
 % *Restituisce l'errore percentuale assoluto (MAPE).*
 
+%% Pulizia dal lavoro precedente
 clear all
 close all
 clc
 
+%% IMPOSTAZIONE PARAMETRI
+validation_year = 2011;
+
+
+%% Preparazione dei dati
+
+% Caricamento dati di interesse.
 load datiOTT
+loads = datiOTT(:,2);
+years = datiOTT(:,3);
 
-% Diamo i nomi alle colonne dei dati
-loads=datiOTT(:,2);
-years=datiOTT(:,3);
+% Estrapolazione delle righe dati dell'ottobre dell'anno di validazione.
+% Il benchmark esclude la previsione del 31 ottobre per la sua irregolarita
+dati_anno_campione = datiOTT(years == validation_year, :);
+numero_giorni_campione = length(dati_anno_campione);
+num_settimane_campione = numero_giorni_campione-8; % Escludo ultima settimana
 
-% Dei dati prendiamo solamente le righe appartenenti al 2011 e ne calcolo
-% il numero.
-dati_anno_campione = datiOTT(years == 2011, :);
-numero_dati_campione = length(dati_anno_campione);
-
+%% Stima errore
 % Usando come dati di validazione di volta in volta tutte le possibili
-% settimane di Ottobre 2011, stimo gli errori, salvandoli in un vettore
-% "errore". Ne faccio poi la media.
-errore = zeros(1, numero_dati_campione);
-for i = 1:(numero_dati_campione-8)
+% settimane di Ottobre dell'anno di validazione, stimo gli errori,
+% salvandoli in un vettore "errore" di cui faccio poi la media.
+erroreMAPE = zeros(1, num_settimane_campione);
+for i = 1:(num_settimane_campione)
     
     datiWeek = dati_anno_campione(i:(i+6),:);
-    stima = predizione(datiWeek);
-    errore(i) = 100*(abs(stima - dati_anno_campione(i+7,2))/dati_anno_campione(i+7,2));
+    
+    L_true = dati_anno_campione(i+7,2);
+    
+    L_hat = predizione(datiWeek);
+    
+    erroreMAPE(i) = 100*(abs(L_hat - L_true)/L_true);
+    
+    % Creazione del vettore che immagazzina il giorno per cui è effettuata
+    % la previsione
     giorni_settimana = datiWeek(:, 6);
     giorno_successivo(i) = giorni_settimana(7)+1;
     if giorno_successivo(i) > 7,
@@ -34,9 +49,15 @@ for i = 1:(numero_dati_campione-8)
     end
 
 end
-media = mean(errore);
+% Calcoolo
+mediaMAPE = mean(erroreMAPE);
 
 % Visualizzo il risultato del benchmark in uns figura.
-figure('NumberTitle', 'off',     'Name', 'Errore MAPE medio')
-bar(errore)
-title(strcat('Errore MAPE - media: ', num2str(media)))
+figure('NumberTitle', 'off',     'Name', 'Errore MAPE')
+    subplot(2, 1, 1)
+    bar(erroreMAPE)
+    title(strcat('Errore MAPE - media: ', num2str(mediaMAPE)))
+
+    subplot(2, 1, 2)
+    bar(giorno_successivo)
+    title(strcat('Giorni della settimana previsti'))
